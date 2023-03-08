7177269f2c5f02c7957dba002cf5fed47a505169
lowell <lowell@mantisnet.com>
2023-03-08 22:27:43 +0100

    change label on PDUSESSIONRELEASECOMPLETEMessageIdentity

--- nasType/NAS_PDUSESSIONRELEASECOMPLETEMessageIdentity.go
+++ nasType/NAS_PDUSESSIONRELEASECOMPLETEMessageIdentity.go
@@ -3,7 +3,7 @@ package nasType
 // PDUSESSIONRELEASECOMPLETEMessageIdentity 9.7
 // MessageType Row, sBit, len = [0, 0], 8 , 8
 type PDUSESSIONRELEASECOMPLETEMessageIdentity struct {
-	Octet uint8 `json:"Octet,omitempty"`
+	Octet uint8 `json:"ID,omitempty"`
 }
 
 func NewPDUSESSIONRELEASECOMPLETEMessageIdentity() (pDUSESSIONRELEASECOMPLETEMessageIdentity *PDUSESSIONRELEASECOMPLETEMessageIdentity) {
